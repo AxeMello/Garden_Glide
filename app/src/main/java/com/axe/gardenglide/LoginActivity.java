bacd4694ff971df128f98be254912ade41c17ccc
axe_mello <akselcruses@gmail.com>
2024-05-19 13:38:00 +0530

    Initial commit

---
+++ app/src/main/java/com/axe/gardenglide/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.axe.gardenglide;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        TextView signupText = findViewById(R.id.signupText);
+        TextView forgotPasswordText = findViewById(R.id.forgotPasswordText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle login logic here
+            }
+        });
+
+        signupText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle sign up logic here
+            }
+        });
+
+        forgotPasswordText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle forgot password logic here
+            }
+        });
+    }
+}
