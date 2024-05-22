8ac8ed59f1cb9eb7eb158972a5322c6435429b5e
axe_mello <akselcruses@gmail.com>
2024-05-22 20:31:32 +0530

    update 7

--- app/src/main/java/com/axe/gardenglide/MarketActivity.java
+++ app/src/main/java/com/axe/gardenglide/MarketActivity.java
@@ -8,12 +8,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MarketplaceActivity extends AppCompatActivity {
+public class MarketActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_marketplace);
+        setContentView(R.layout.activity_market);
 
         // Initialize views
         ImageView user = findViewById(R.id.user);
